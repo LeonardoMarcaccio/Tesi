09aef413db2a5821a897bccbcd8c10e5d1db812f
Leonardo Marcaccio <Leonardo Marcaccio>
2025-02-14 16:37:02 +0100

    Fine prima revisione

--- listings/BTree/BTreeSplit.cs
+++ listings/BTree/BTreeSplit.cs
@@ -1,19 +1,22 @@
-internal (Page? Page, byte[] PageBuffer) GetPage(uint pageID)
+private void SplitChild(NodePage<TK, TP> parentNode, int nodeToBeSplitIndex, NodePage<TK, TP> nodeToBeSplit)
 {
-    if (BufferedPages.TryGetValue(pageID, out var page))
+    NodePage<TK, TP> newNode = CreateNewNode();
+
+    parentNode.InsertEntry(nodeToBeSplitIndex, nodeToBeSplit.GetEntry(this.Degree - 1));
+    parentNode.InsertChild(nodeToBeSplitIndex + 1, newNode);
+
+    newNode.AppendEntries(nodeToBeSplit.GetEntriesRange(this.Degree, this.Degree - 1));
+
+    // remove also _entries[this.Degree - 1], which is the one to move up to the parent
+    nodeToBeSplit.RemoveEntriesRange(this.Degree - 1, this.Degree);
+
+    if (!nodeToBeSplit.IsLeaf)
     {
-        return (page, new byte[0]);
+        newNode.AppendChildren(nodeToBeSplit.GetChildrenRange(this.Degree, this.Degree));
+        nodeToBeSplit.RemoveChildrenRange(this.Degree, this.Degree);
     }
 
-    byte[] buffer = new byte[SysConstants.PAGE_SIZE];
-    long pageStart = SysConstants.PAGE_SIZE * pageID;
-
-    if (this._fileStream.Length < pageStart + SysConstants.PAGE_SIZE)
-    {
-        throw new ArgumentOutOfRangeException($"GetPage, pageNumber: {pageID}, FileStream.Length: {this._fileStream.Length}, PAGE_SIZE: {SysConstants.PAGE_SIZE}");
-    }
-
-    this._fileStream.Position = pageStart;
-    this._fileStream.Read(buffer, 0, SysConstants.PAGE_SIZE);
-    return (null, buffer);
+    parentNode.UpdateEntryReferences();
+    newNode.UpdateEntryReferences();
+    nodeToBeSplit.UpdateEntryReferences();
 }
